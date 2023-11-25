4467e3299954e3fe05f837de4de940ac6d9be01b
Cruisero <cruisebroo@gmail.com>
2023-11-25 12:45:11 +0800

    管理后台-订单详情

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -21,6 +21,7 @@ Route::group([
     $router->put('products/{id}', 'ProductsController@update');
     // $router->put('products/{id}/edit', 'ProductsController@Delete');
     $router->delete('products/{id}', 'ProductsController@destroy');
-    $router->get('orders', 'OrdersController@index')->name('orders.index');
+    $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
+    $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
 
 });
