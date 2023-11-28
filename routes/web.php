cb83984d1e5b747b2775d93d1a00f6d411ef84d3
Cruisero <cruisebroo@gmail.com>
2023-11-28 12:34:38 +0800

    管理后台 - 拒绝退款

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', 'PagesController@root')->name('root');
+// Route::get('/', 'PagesController@root')->name('root');
 // Route::get('/', function () {
 //     return view('app');
 // })
